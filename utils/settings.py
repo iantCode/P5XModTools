a88a2badaef5a4753e4898d4e5d8eb843065691f
iant <iant@iant.kr>
2025-01-16 16:55:07 +0900

    v0.2.0

--- utils/settings.py
+++ utils/settings.py
@@ -21,7 +21,7 @@ def get_drives():
 
 class Setting(SingletonInstance):
     def __init__(self):
-        self.program_version = "0.1.7"
+        self.program_version = "0.2.0"
 
         self.game_location: str = ""
         self.region: Region = Region.CN
